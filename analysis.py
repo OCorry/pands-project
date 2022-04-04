673e745b0b42ae424350c44d98c49a16cd34ade7
Orla <G00411336@gmit.ie>
2022-04-04 22:31:44 +0100

    iris.csv

--- analysis.py
+++ analysis.py
@@ -2,5 +2,5 @@ import sys
 with open(sys.argv[1], 'rt') as f:
     data = f.read()  #storing f.read() in a variable called data so it is more accessable
 print (data)
-
+#dataset source https://datahub.io/machine-learning/iris#data
 #checking that I have saved the dataset correcly and checking if i can read it in from the command line
