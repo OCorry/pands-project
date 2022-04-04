0a6bb26327b8e5bc2ab55bd7398f6712913b83ee
Orla <G00411336@gmit.ie>
2022-04-04 22:24:37 +0100

    iris.csv

--- analysis.py
+++ analysis.py
@@ -3,4 +3,4 @@ with open(sys.argv[1], 'rt') as f:
     data = f.read()  #storing f.read() in a variable called data so it is more accessable
 print (data)
 
-#checking that I have saved the dataset correcly and checking if i can read it in from the command line 
+#checking that I have saved the dataset correcly and checking if i can read it in from the command line
