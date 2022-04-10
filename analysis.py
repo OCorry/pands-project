ccab79f520fd31ee763627215c6fc14cdf14a089
Orla <G00411336@gmit.ie>
2022-04-10 17:08:08 +0100

    pands-project

--- analysis.py
+++ analysis.py
@@ -4,25 +4,37 @@ import sys
 with open(sys.argv[1], 'rt') as f:
     data = f.read()  #storing f.read() in a variable called data so it is more accessable
 print (data)
-#dataset source https://datahub.io/machine-learning/iris#data
+#dataset source: https://www.kaggle.com/datasets/saurabh00007/iriscsv?resource=download
 #checking that I have saved the dataset correcly and checking if i can read it in from the command line
 '''
-import pandas as pd  #using Pandas library to read the file into Python as a comma seperated values(csv) file
-import numpy as np
-import matplotlib.pyplot as plt 
-import seaborn as sns
+
+#importing libraries 
+
+import pandas as pd  #to analysie data that is in tabular form
+import numpy as np #for working with data stored as arrays
+import matplotlib.pyplot as plt #plotting data 
+import seaborn as sns #plotting data 
+
+
+#reading in the dataset
 filename =("iris.csv")
-df = pd.read_csv (filename)
+df = pd.read_csv (filename) #reading in the file to Python using Pandas
 #print (df)
+
 #print(df.to_string()) #use this code to print the whole dataframe (df) rather than just the first 5 & last 5 rows
                     #https://www.w3schools.com/python/pandas/pandas_csv.asp
 
+shape = df.shape #storing as variable shape
+describe = df.describe() #storing as variable describe
 
-#sns.countplot(x = 'class', data =df,) #to show how many of each class(or type) of flower there are in the dataframe)
+print (shape) #gives the number of columns and rows of the dataframe 
+print (describe) #distribution of data - eg, mean, standard deviation and max & min values
+
+sns.set_theme(style="darkgrid")  #https://seaborn.pydata.org/generated/seaborn.barplot.html
+sns.countplot(x = 'Species', data =df) #to show how many of each sample of flower there are in the dataframe
 #plt.show()                              #https://www.geeksforgeeks.org/countplot-using-seaborn-in-python/
 
-#plt.savefig('class_count.png')
-
+plt.savefig('species_count.png')
 
 
 
