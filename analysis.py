5c5ac8ae445d507fe6fcb90e74c52c9cac5c35ab
Orla <G00411336@gmit.ie>
2022-04-13 22:22:48 +0100

    pands-project

--- analysis.py
+++ analysis.py
@@ -1,4 +1,4 @@
-#Program that reads the Iris Dataset into Python and run analysis on the dataset 
+#Program that reads the Iris Dataset into Python and run analysis on the dataset
 #Author: Orla Corry 
 
 
@@ -18,18 +18,27 @@ df = pd.read_csv (filename) #reading in the file to Python using Pandas
 #print(df.to_string()) #use this code to print the whole dataframe (df) rather than just the first 5 & last 5 rows
                     #https://www.w3schools.com/python/pandas/pandas_csv.asp
 
-shape = df.shape #storing as variable shape
-describe = df.describe() #storing as variable describe
 
-print (shape) #gives the number of columns and rows of the dataframe 
-print (describe) #distribution of data - eg, mean, standard deviation and max & min values
 
 sns.set_theme(style="darkgrid")  #https://seaborn.pydata.org/generated/seaborn.barplot.html
 sns.countplot(x = 'Species', data =df) #to show how many of each sample of flower there are in the dataframe
 #plt.show()                              #https://www.geeksforgeeks.org/countplot-using-seaborn-in-python/
 
-plt.savefig('species_count.png')
+#plt.savefig('species_count.png')
 
+shape = df.shape #storing as variable shape
+describe = df.describe() #storing as variable describe
+
+with open ("variable_summary.txt" , 'wt') as f: #creating a .txt file  as 'f'which i can write to 
+    #from now on the file will be referred to as 'f'
+
+    f.write('Analysis: Shape of Dataframe:') #writing a heading 'shape of dataframe and printing out result
+    print("\nThe shape of this dataframe is:", shape, file =f) #\n here so output will print below heading
+    #the first argument is the command shape and the second argument is to sent it to file 'f'
+    f.write('\n\n\nAnalysis: Description of Dataframe:') #using \n*3 so this prints 3 lines below the first output
+    print ("\n", describe, file =f) #\n here so output will print below heading
+
+    #https://stackoverflow.com/questions/36571560/directing-print-output-to-a-txt-file - accessed 13/04
 
 
 
