dcc9184820991b02db3f3a35454fe21b524ed78d
Orla <G00411336@gmit.ie>
2022-04-21 22:06:37 +0100

    pands-project

--- analysis.py
+++ analysis.py
@@ -23,7 +23,7 @@ df = pd.read_csv (filename) #reading in the file to Python using Pandas
 shape = df.shape #storing as variable shape
 describe = df.describe() #storing as variable describe
 
-with open ("variable_summary.txt" , 'wt') as f: #creating a .txt file  as 'f'which i can write to 
+with open ("variable_summary.txt" , 'at') as f: #creating a .txt file  as 'f'which i can write to 
     #from now on the file will be referred to as 'f'
 
     f.write('\t\t\t\t\t\t\tAnalysis:') #Heading-using \t to tab to centre 
@@ -95,7 +95,7 @@ with open ("variable_summary.txt" , 'wt') as f: #creating a .txt file  as 'f'whi
 
     #printing from the Petal Lenth & Width columns and only printing the Iris-versicolor data - using code [50:100]
     print("\n\nPetal Length and Petal Width Analysis of Iris-versicolor:\n", file=f)
-    print(df[["PetalLengthCm",]][50:100].describe(), file = f) #using describe() to get analysis of the specidied column and rows
+    print(df[["PetalLengthCm",]][50:100].describe(), file = f) #using describe() to get analysis of the specified column and rows
     print(df[["PetalWidthCm",]][50:100].describe(), file = f)
 
 
@@ -111,15 +111,38 @@ with open ("variable_summary.txt" , 'wt') as f: #creating a .txt file  as 'f'whi
     print(df[["PetalWidthCm",]][100:150].describe(), file = f)
 
 
+#Histogram of Sepal Length 
+df.hist(column ="SepalLengthCm", grid=True, color="blue") #specifically picking out the sepal length attribute
+plt.title("Sepal Length") #labling the title
+plt.xlabel('Sepal Length in cm', color ="green") #labling the x axis 
+plt.ylabel('Frequency', color ="green") #labling the y axis
+plt.savefig("Sepal Length.png") #saving the hist as a .png file 
+#plt.show() #commenting this out - if not commented out it will print a hist in the terminal 
 
+#Histpgram of Petal width 
+df.hist(column ="SepalWidthCm", grid=True, color="blue",)
+plt.title("Sepal Width")
+plt.xlabel('Sepal width in cm', color ="green")
+plt.ylabel('Frequency', color ="green")
+plt.savefig("Sepal Width.png")
+#plt.show()
+
+#Histogram of Petal Length
+df.hist(column ="PetalLengthCm", grid=True, color="blue",) 
+plt.title("Petal Length")
+plt.xlabel('Petal Length in cm', color ="green")
+plt.ylabel('Frequency', color ="green")
+plt.savefig("Petal Length.png")
+#plt.show()
+
+#Histogram of Petal width
+df.hist(column ="PetalWidthCm", grid=True, color="blue",)
+plt.title("Petal Width")
+plt.xlabel('Petal Width in cm', color ="green")
+plt.ylabel('Frequency', color ="green")
+plt.savefig("Petal Width.png")
+#plt.show()
     
-
-
-
-
-
-
-
 
 
 
